de7bb17c4b82213b58d6fb5c9a2f0a6fb8b47957
ben <roberto.song940726@gmail.com>
2024-08-14 03:59:32 -0700

    change button caption

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,8 +3,8 @@ import { Button } from "antd";
 export default function Header() {
     return (
         <div className="fixed h-[60px] pl-[256px] shadow-sm w-full flex items-center justify-end">
-            <Button shape="round mr-[8px]" type="primary">Login</Button>
-            <Button shape="round mr-[8px]" type="primary">Register</Button>
+            <Button shape="round mr-[8px]" type="primary">登录</Button>
+            <Button shape="round mr-[8px]" type="default">注册</Button>
         </div>
     )
 }
