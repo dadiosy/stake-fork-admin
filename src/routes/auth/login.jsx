b43f79d1f3f6e03d4ec1fd6cc06678f39387030f
ben <roberto.song940726@gmail.com>
2024-08-15 00:37:14 -0700

    add login page

--- src/routes/auth/login.jsx
+++ src/routes/auth/login.jsx
@@ -1,5 +1,54 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input, Flex, Card } from 'antd';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 export default function LoginPage() {
+    const navigate = useNavigate()
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+        navigate('/')
+    };
+
     return (
-        <div>login </div>
+        <div className="h-screen bg-main-darkest flex flex-col justify-center items-center">
+            <h5 className="text-h5 text-white mb-[20px]">后台管理系统登录</h5>
+            <Card className='md:w-[400px]'>
+                <Form
+                    name="login"
+                    initialValues={{ remember: true }}
+                    style={{ maxWidth: 360 }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your Username!' }]}
+                    >
+                        <Input prefix={<UserOutlined />} placeholder="Username" size='large' />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your Password!' }]}
+                    >
+                        <Input prefix={<LockOutlined />} type="password" placeholder="Password" size='large' />
+                    </Form.Item>
+                    <Form.Item>
+                        <Flex justify="space-between" align="center">
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>记住</Checkbox>
+                            </Form.Item>
+                            <Link to="/auth/reset-password">忘记密码</Link>
+                        </Flex>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button block type="primary" htmlType="submit" size='large'>
+                            登录
+                        </Button>
+                        <Link to="/auth/register" className='text-blue-500'>注册新账号</Link>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div >
     )
 }
