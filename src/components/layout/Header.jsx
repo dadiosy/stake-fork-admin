d19a792e4d4a1c2a67ef9b68f3c9b3cafe869714
ben <roberto.song940726@gmail.com>
2024-08-14 22:26:49 -0700

    add login and register page skeleton

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,11 @@
 import { Button } from "antd";
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
         <div className="fixed h-[60px] pl-[256px] shadow-md w-full flex items-center justify-end bg-white z-[9998]">
-            <Button shape="round mr-[8px]" type="primary">登录</Button>
-            <Button shape="round mr-[8px]" type="default">注册</Button>
+            <Button shape="round mr-[8px]" type="primary"><Link to="/auth/login">登录</Link></Button>
+            <Button shape="round mr-[8px]" type="default"><Link to="/auth/register">注册</Link></Button>
         </div>
     )
 }
--- src/router.js
+++ src/router.js
@@ -5,8 +5,13 @@ import Layout from "./routes/Layout";
 import ErrorPage from "./error-page";
 import UserData from "./routes/user/UserData";
 import Dashboard from "./routes/dashboard";
+import LoginPage from "./routes/auth/login";
 
 const router = createBrowserRouter([
+    {
+        path: '/auth/login',
+        element: <LoginPage />
+    },
     {
         path: "/",
         element: <Layout />,
