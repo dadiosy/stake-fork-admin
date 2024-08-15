851c643d992b1f05ee3175932c237db68d3bd77d
ben <roberto.song940726@gmail.com>
2024-08-14 21:50:20 -0700

    add bg to the header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import { Button } from "antd";
 
 export default function Header() {
     return (
-        <div className="fixed h-[60px] pl-[256px] shadow-sm w-full flex items-center justify-end">
+        <div className="fixed h-[60px] pl-[256px] shadow-sm w-full flex items-center justify-end bg-white">
             <Button shape="round mr-[8px]" type="primary">登录</Button>
             <Button shape="round mr-[8px]" type="default">注册</Button>
         </div>
