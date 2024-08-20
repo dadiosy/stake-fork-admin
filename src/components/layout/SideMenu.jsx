2032d6372c7f3127c72f93f05a11a26e03ebf348
lovemonday-dev <bluespider0102@gmail.com>
2024-08-20 17:42:13 +0900

    remove unnecessary links in sidebar

--- src/components/layout/SideMenu.jsx
+++ src/components/layout/SideMenu.jsx
@@ -35,83 +35,83 @@ export default function SideMenu({ menuWidth = MENU_WIDTH, collapsed = false }) 
                     key: 'user-data',
                     label: '用户管理',
                 },
-                {
-                    key: 'register-data',
-                    label: '注册管理',
-                },
-                {
-                    key: 'login-data',
-                    label: '登入管理',
-                },
+                // {
+                //     key: 'register-data',
+                //     label: '注册管理',
+                // },
+                // {
+                //     key: 'login-data',
+                //     label: '登入管理',
+                // },
             ],
         },
-        {
-            key: 'history-data',
-            label: '历史数据管理',
-            icon: <HistoryOutlined />,
-            children: [
-                {
-                    key: 'topup_detail',
-                    label: '充值历史数据',
-                },
-                {
-                    key: 'cash_detail',
-                    label: '提款明细数据',
-                },
-                {
-                    key: 'tansaction_detail',
-                    label: '交易明细数据',
-                },
-                {
-                    key: 'bet_detail',
-                    label: '投注明细数据',
-                },
-            ],
-        },
-        {
-            key: 'bet',
-            label: '投注管理',
-            icon: <MoneyCollectOutlined />,
-            children: [
-                { key: '5', label: 'xx管理' },
-                { key: '6', label: 'xx管理' },
-                {
-                    key: 'sub3',
-                    label: '子菜单',
-                    children: [
-                        { key: '7', label: 'xx管理' },
-                        { key: '8', label: 'xx管理' },
-                    ],
-                },
-            ],
-        },
-        {
-            key: 'game',
-            label: '游戏管理',
-            icon: <AppstoreOutlined />,
-            children: [
-                { key: '9', label: 'xx管理' },
-                { key: '10', label: 'xx管理' },
-                { key: '11', label: 'xx管理' },
-                { key: '12', label: 'xx管理' },
-            ],
-        },
-        {
-            key: 'security',
-            label: '安全管理',
-            icon: <SecurityScanOutlined />,
-            children: [
-                { key: '13', label: 'xx管理' },
-            ],
-        },
-        {
-            key: 'proxy',
-            label: '代理管理',
-            icon: <SettingOutlined />,
-            children: [
-                { key: '14', label: 'xx管理' },
-            ],
-        },
+        // {
+        //     key: 'history-data',
+        //     label: '历史数据管理',
+        //     icon: <HistoryOutlined />,
+        //     children: [
+        //         {
+        //             key: 'topup_detail',
+        //             label: '充值历史数据',
+        //         },
+        //         {
+        //             key: 'cash_detail',
+        //             label: '提款明细数据',
+        //         },
+        //         {
+        //             key: 'tansaction_detail',
+        //             label: '交易明细数据',
+        //         },
+        //         {
+        //             key: 'bet_detail',
+        //             label: '投注明细数据',
+        //         },
+        //     ],
+        // },
+        // {
+        //     key: 'bet',
+        //     label: '投注管理',
+        //     icon: <MoneyCollectOutlined />,
+        //     children: [
+        //         { key: '5', label: 'xx管理' },
+        //         { key: '6', label: 'xx管理' },
+        //         {
+        //             key: 'sub3',
+        //             label: '子菜单',
+        //             children: [
+        //                 { key: '7', label: 'xx管理' },
+        //                 { key: '8', label: 'xx管理' },
+        //             ],
+        //         },
+        //     ],
+        // },
+        // {
+        //     key: 'game',
+        //     label: '游戏管理',
+        //     icon: <AppstoreOutlined />,
+        //     children: [
+        //         { key: '9', label: 'xx管理' },
+        //         { key: '10', label: 'xx管理' },
+        //         { key: '11', label: 'xx管理' },
+        //         { key: '12', label: 'xx管理' },
+        //     ],
+        // },
+        // {
+        //     key: 'security',
+        //     label: '安全管理',
+        //     icon: <SecurityScanOutlined />,
+        //     children: [
+        //         { key: '13', label: 'xx管理' },
+        //     ],
+        // },
+        // {
+        //     key: 'proxy',
+        //     label: '代理管理',
+        //     icon: <SettingOutlined />,
+        //     children: [
+        //         { key: '14', label: 'xx管理' },
+        //     ],
+        // },
         {
             key: 'logs',
             label: '日记管理',
